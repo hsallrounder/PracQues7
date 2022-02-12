85ed62ccb136d46f2e6e3e18fee74624eaf72ecf
Himanshu Sharma <hsallrounder@gmail.com>
2022-02-12 15:30:57 +0530

    Method Updated

--- src/Q4.java
+++ src/Q4.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Q4 {
+    // We can solve the problem by modifying the BubbleSort Algo
     public void even_odd_arr(int[] a){
         int n=a.length;
         for(int i=0;i<n-1;i++){
@@ -14,6 +15,28 @@ public class Q4 {
             }
         }
     }
+    /* Another Method solve the question*/
+//    public int[] even_odd_arr(int[] a){
+//        int n=a.length;
+//        int arr[]=new int[n];
+//        int i=0,k=0;
+//        while(i<n){
+//            if(a[i]%2==0) {
+//                arr[k]=a[i];
+//                k++;
+//            }
+//            i++;
+//        }
+//        i=0;
+//        while (i<n){
+//            if(a[i]%2!=0){
+//                arr[k]=a[i];
+//                k++;
+//            }
+//            i++;
+//        }
+//        return arr;
+//    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of your array: ");
@@ -25,6 +48,9 @@ public class Q4 {
         }
         sc.close();
         new Q4().even_odd_arr(arr);
+        /*using another method
+        arr=new Q4().even_odd_arr(arr);
+         */
         System.out.println(Arrays.toString(arr));
     }
 }
